5d4b57f84f00748129e11ef62775c1cc2ad7d360
User -2022RYNXA\Administrator <hsykrw@naver.com>
2022-09-28 18:07:12 +0900

    no message

--- JS/js_220928/js_7_배열/배열.js
+++ JS/js_220928/js_7_배열/배열.js
@@ -1,9 +1,19 @@
 'use strict';
 
-const a = [70, 60, 55, 90, 80];
-let min= 100, max =0;
+// const a = [70, 60, 55, 90, 80];
+// let min= 100, max =0;
 
-for (let i = 0; i < a.length; i++) {
+// for (let i = 0; i < a.length; i++) {
+//     if (a[i]>max) max=a[i];
+//     if (a[i]<min) min=a[i];
+// }
+
+// console.log(max + ' ' + min);
+
+const a = [70, 60, 55, 90, 80];
+let min= a[0], max = a[0];
+
+for (let i = 1; i < a.length; i++) {
     if (a[i]>max) max=a[i];
     if (a[i]<min) min=a[i];
 }
