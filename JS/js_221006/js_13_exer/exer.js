22ae19ef915d80baa84e5372bd7d407318f6e867
User -2022RYNXA\Administrator <hsykrw@naver.com>
2022-10-07 17:34:45 +0900

    no message

--- JS/js_221006/js_13_exer/exer.js
+++ JS/js_221006/js_13_exer/exer.js
@@ -3,41 +3,29 @@
 let name, kor, eng, math;
 let sum = 0;
 
-const subject = ['국어 점수', '영어 점수', '수학 점수'];
-const score = [];
+let subject = [
+    ['학생 명', '국어 점수', '영어 점수', '수학 점수', '합계', '평균'],
+];
+subject[1] = new Array(subject[0].length);
+subject[1][subject[1].length - 2] = 0;
 
-name = prompt(`학생 명 :`);
-for (let i = 0; i < subject.length; i++) {
-    score[i] = +prompt(`${subject[i]}`);
-    sum += score[i];
-
-    if(score[i] > 100 || score[i] < 0 ) {
-        alert(`0부터 100 까지 입력`);
-        break;
+for (let i = 0; i < subject[0].length - 2; i++) {
+    subject[1][i] = prompt(subject[0][i]);
+    if (i > 0) {
+        subject[1][subject[0].length - 2] += +subject[1][i];
     }
 }
+subject[1][subject[1].length - 1] = Math.trunc(subject[1][subject[1].length - 2] / (subject[1].length - 3));
 
-
-let avr = sum / subject.length;
-let result = Math.trunc(avr);
 
 document.write(`<table>`);
-document.write(`<caption> < ${name} 학생의 성적 > </caption>`);
-for (let i = 0; i < subject.length; i++) {
+document.write(`<caption> < ${subject[1][0]} 학생의 성적 > </caption>`);
+for (let i = 1; i < subject[0].length; i++) {
     document.write(`
         <tr>
-            <th>${subject[i]}</th>
-            <td>${score[i]}점</td>
+        <th>${subject[0][i]}</th>
+        <td>${subject[1][i]}점</td>
         </tr>
         `)
 }
-
-document.write(`<tr>
-            <th>합계</th>
-            <td>${sum}점</td>
-        </tr>`);
-document.write(`<tr>
-            <th>평균</th>
-            <td>${result}점</td>
-        </tr>`);
-document.write(`</table >`);
+document.write(`</table>`);
