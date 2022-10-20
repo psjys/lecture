bd56b1e2864a71a500848b19b8d185c6f785e8ad
User -2022RYNXA\Administrator <hsykrw@naver.com>
2022-10-20 16:52:31 +0900

    dd

--- JS/js_221019/js_5_exer/exer.js
+++ JS/js_221019/js_5_exer/exer.js
@@ -1,8 +1,8 @@
 /* 
 3분의1 스크롤되는 시점부터 탑버튼 생성 
-v opacity 적용 
-v hover 하면 너비 늘어남 + opacity1 
-v 손가락 생기기 
+opacity 적용 
+hover 하면 너비 늘어남 + opacity1 
+손가락 생기기 
 위로 천천히 
 3분의1 지점 돌아가면 다시 없어짐
 */
@@ -10,38 +10,29 @@ v 손가락 생기기
 'use strict';
 
 let topBtn = document.querySelector('.scrollTop');
-let body = document.querySelector('body');
-let scrollY;
 
 document.addEventListener('scroll', () => {
-    scrollY = parseInt(window.scrollY);
-
-    if (scrollY >= body.scrollHeight / 3) {
-        topBtn.style.opacity = .5;
-        topBtn.style.transition = 'all .5s';
-        topBtn.style.visibility = 'visible';
+    if (parseInt(window.scrollY) >= innerHeight / 3) {
+        topBtn.classList.add('scroll');
     } else {
-        topBtn.style.opacity = 0;
-        topBtn.style.visibility = 'hidden';
+        topBtn.classList.remove('scroll');
     }
 })
 
-topBtn.addEventListener('mouseenter' , () => {
-    topBtn.style.opacity = '1';
-    topBtn.style.width = '80px';
-    topBtn.style.cursor = 'pointer';
+topBtn.addEventListener('mouseenter', () => {
+    topBtn.classList.add('btnhover');
 });
 
 topBtn.addEventListener('mouseleave', () => {
-    topBtn.style.opacity = .5;
-    topBtn.style.width = '40px';
+    topBtn.classList.remove('btnhover');
 });
 
 topBtn.addEventListener('click', () => {
     let id = setInterval(function () {
-        window.scroll(0, scrollY -= 5);
-        if(scrollY <= 0) {
+        window.scrollBy(0, -5);
+        if (parseInt(window.scrollY) <= 0) {
             clearInterval(id);
         }
     }, 5);
 });
+
---
+++ JS/js_221020/js_1_exer/exer.js
@@ -0,0 +1,35 @@
+/* 
+이미지 화면 중앙 + radius
+버튼 hover 했을 때 진하게
+클릭 할 때 옆에 버튼 생성 + 슬라이드
+마지막 페이지에 버튼 없어짐
+*/
+
+'use strict';
+
+
+let button = document.getElementsByClassName('button'),
+    imgBlock = document.getElementsByClassName('imgBlock'),
+    v = 0;
+
+button[1].addEventListener('click', function () {
+    v += 100;
+    if(v == 100) {
+        button[0].style.visibility = 'visible';
+    }
+    imgBlock[0].style.right = `${v}%`;
+    if(v == 400) {
+        button[1].style.visibility = 'hidden';
+    }
+});
+
+button[0].addEventListener('click', function() {
+    v -= 100;
+    if(v == 0) {
+        button[0].style.visibility = 'hidden';
+    }
+    imgBlock[0].style.right = `${v}%`;
+    if(v == 300) {
+        button[1].style.visibility = 'visible';
+    }
+}) 
