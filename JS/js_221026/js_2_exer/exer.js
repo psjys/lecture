ecb1e7e17cb333dbae234b97fc058719e911e2dd
User -2022RYNXA\Administrator <hsykrw@naver.com>
2022-10-28 19:39:35 +0900

    no message

--- JS/js_221026/js_2_exer/exer.js
+++ JS/js_221026/js_2_exer/exer.js
@@ -8,39 +8,124 @@
 // 이벤트 위임 버전으로 고치기
 // contents ~~ 효율적으로 고치기 (이전)
 
+// 'use strict';
+
+// const header = document.querySelector('header');
+// const menuBtn = header.getElementsByTagName('a');
+// const contents = header.querySelector ('.contents');
+// const main = document.querySelector('main');
+
+// let beforeBtn = menuBtn[0],
+//     beforeHref;
+
+// function setMain () {
+//     main.style.marginTop = header.offsetHeight + 'px';
+// }
+// function viewContent (self) {
+//     contents.querySelector(beforeBtn.getAttribute('href')).style.display = 'none';
+//     contents.querySelector(self.getAttribute('href')).style.display = 'block';
+// }
+// viewContent(beforeBtn);
+// setMain();
+
+// for (let i = 0; i < menuBtn.length; i++) {
+//     menuBtn[i].addEventListener('click', function () {
+//         beforeBtn.classList.remove('selected');
+//         this.classList.add ('selected');
+
+//         viewContent(this);
+
+//         setMain();
+//         beforeBtn = this;
+//     });
+// }
+
+// window.addEventListener('resize', setMain);
+
+
+// 'use strict'
+
+// let main = document.querySelector('main'),
+//     header = document.querySelector('header'),
+//     ul = header.querySelector('ul'),
+//     menuBtn = ul.querySelectorAll('a'),
+//     contents = header.querySelector('.contents');
+
+// let beforeBtn = menuBtn[0];
+// let beforeBox = contents.querySelector('div');
+
+// function setMain() {
+//     main.style.marginTop = header.offsetHeight + 'px';
+// }
+// function display (x) {
+//     beforeBox.style.display = x;
+// }
+
+// display('block');
+
+// setMain();
+
+// ul.addEventListener('click', function (e) {
+//     let oj = e.target;
+//     let currentBox = contents.querySelector(oj.getAttribute('href'));
+
+//     if (oj.tagName == 'A') {
+
+//         beforeBtn.classList.remove('selected');
+//         oj.classList.add('selected');
+
+//         display ('none'); 
+//         currentBox.style.display = 'block';
+
+//         beforeBtn = oj;
+//         beforeBox = currentBox;
+
+//         setMain();
+//     }
+// });
+
+// window.addEventListener('resize', setMain);
+
+
+
+// sticky 버전
+
 'use strict';
 
-const header = document.querySelector('header');
-const menuBtn = header.getElementsByTagName('a');
-const contents = header.querySelector ('.contents');
-const main = document.querySelector('main');
+let main = document.querySelector('main'),
+    header = document.querySelector('header'),
+    ul = header.querySelector('ul'),
+    menuBtn = ul.querySelectorAll('a'),
+    contents = header.querySelector('.contents');
 
-let beforeBtn = menuBtn[0],
-    beforeHref;
+let beforeBtn = menuBtn[0];
+let beforeBox = contents.querySelector('div');
 
-function setMain () {
-    main.style.marginTop = header.offsetHeight + 'px';
+function display(x) {
+    beforeBox.style.display = x;
 }
-function viewContent (self) {
-    contents.querySelector(beforeBtn.getAttribute('href')).style.display = 'none';
-    contents.querySelector(self.getAttribute('href')).style.display = 'block';
-}
-viewContent(beforeBtn);
-setMain();
 
-for (let i = 0; i < menuBtn.length; i++) {
-    menuBtn[i].addEventListener('click', function () {
+display('block');
+
+ul.addEventListener('click', function (e) {
+    let oj = e.target;
+    let currentBox = contents.querySelector(oj.getAttribute('href'));
+    e.preventDefault();
+
+    if (oj.tagName == 'A') {
+
         beforeBtn.classList.remove('selected');
-        this.classList.add ('selected');
-        
-        viewContent(this);
+        oj.classList.add('selected');
 
-        setMain();
-        beforeBtn = this;
-    });
-}
+        display('none');
+        currentBox.style.display = 'block';
 
-window.addEventListener('resize', setMain);
-// window.addEventListener('resize', {
-//
-// })
+        beforeBtn = oj;
+        beforeBox = currentBox;
+    }
+});
+
+// sticky 다음에 발생하게 
+setTimeout(function () {
+    window.scroll(0, 0);
+}, .1);
