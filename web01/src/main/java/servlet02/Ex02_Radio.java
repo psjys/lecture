cf5d69c1464245825e6a4b6f98e3305d9f424b80
116 <hsykrw@naver.com>
2022-12-26 18:10:18 +0900

    no message

--- web01/src/main/java/servlet02/Ex02_Radio.java
+++ web01/src/main/java/servlet02/Ex02_Radio.java
@@ -1,40 +1,41 @@
 package servlet02;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Ex02_Radio
- */
 @WebServlet("/radio")
 public class Ex02_Radio extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public Ex02_Radio() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String gender = request.getParameter("gender");
+		String mailcheck = request.getParameter("mailcheck");
+		String content = request.getParameter("content");
+		
+		response.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		out.print("<html><body><h2>");
+		out.print("** Radio & TextArea Test **</h2>");
+		out.print("<h3>** 성별 : " + gender+"</h3>");
+		out.print("<h3>** 메일 : " + mailcheck+"</h3>");
+		out.print("<h3>** 인사 : " + content+"</h3><br><br>");
+		out.print("<a href='/web01/servletTestForm/form02_Radio.jsp'> 다시 입력하기 </a>");
+		out.print("</body></html>");
+	
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
