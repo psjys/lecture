959b36d6f9af1e5565887a77f426d2bea37c07ef
116 <hsykrw@naver.com>
2023-01-10 01:27:34 +0900

    no message

--- main/src/main/Main.java
+++ main/src/main/Main.java
@@ -1,12 +1,18 @@
 package main;
 
+import java.util.Scanner;
+
 class Main {
     public static void main(String [] arg) {
-        System.out.println("         ,r'\"7");
-        System.out.println("r`-_   ,'  ,/");
-        System.out.println(" \\. \". L_r'");
-        System.out.println("   `~\\/");
-        System.out.println("      |");
-        System.out.println("      |");
+        Scanner sc = new Scanner();
+        int a , b;
+        a = nextInt();
+        b = nextInt();
+
+        System.out.println((b%10)*a);
+        System.out.println((b/10 - ((b/100)*10))*a);
+        System.out.println((b/100) * a);
+        
+
     }
 }
---
+++ untitled/src/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        if (x > 0 && y > 0) {
+            System.out.print(1);
+        } else if (x < 0 && y > 0) {
+            System.out.print(2);
+        } else if (x < 0 && y < 0) {
+            System.out.print(3);
+        } else {
+            System.out.print(4);
+        }
+    }
+}
