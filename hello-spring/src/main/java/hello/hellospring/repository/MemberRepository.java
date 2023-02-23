dd0077988031253424426051af772e0fc83b56fb
116 <hsykrw@naver.com>
2023-02-23 18:01:11 +0900

    no message

--- hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,10 +1,7 @@
 package hello.hellospring.repository;
-
 import hello.hellospring.domain.Member;
-
 import java.util.List;
 import java.util.Optional;
-
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
